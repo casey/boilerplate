67fd9968f1935158399991338411afe9391a54d4
Casey Rodarmor <casey@rodarmor.com>
2022-09-11 00:51:40 -0700

    Parse macro input with syn::parse_macro_input

--- src/lib.rs
+++ src/lib.rs
@@ -285,7 +285,7 @@ use {
   proc_macro2::{Span, TokenStream},
   quote::{quote, ToTokens, TokenStreamExt},
   std::path::Path,
-  syn::{DeriveInput, Ident, LitStr},
+  syn::{parse_macro_input, DeriveInput, Ident, LitStr},
 };
 
 mod block;
@@ -295,8 +295,7 @@ mod template;
 
 #[proc_macro_derive(Display, attributes(display))]
 pub fn display(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-  let derive_input = syn::parse2::<DeriveInput>(TokenStream::from(item))
-    .expect("Failed to parse token stream into derive input");
+  let derive_input = parse_macro_input!(item as DeriveInput);
 
   Display::from_derive_input(&derive_input)
     .expect("Failed to parse derive input")
