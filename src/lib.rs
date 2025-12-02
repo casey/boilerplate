1b7af6d3253a74eea03c3b6d9fe4c2cd5c99e296
Casey Rodarmor <casey@rodarmor.com>
2025-12-01 16:23:58 -0800

    Amend

--- src/lib.rs
+++ src/lib.rs
@@ -411,8 +411,8 @@
 //! and an implementation of the `Boilerplate` trait. Normally the
 //! `Boilerplate` trait and its implementation can be ignored, but when the
 //! `reload` feature is enabled, The `Boilerplate` trait includes
-//! `Boilerplate::reload`, which takes a new template string, and allows you to
-//! reload the template at runtime.
+//! `Boilerplate::reload`, which takes a new template string to reload a
+//! template at runtime.
 //!
 //! Boilerplate templates contain Rust code which is compiled ahead of time.
 //! Consequently, the new template's code blocks must match those of the
