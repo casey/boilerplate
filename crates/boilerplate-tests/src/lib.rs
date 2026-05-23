96b6d16dda3d46924d555821fff7766adfad0b88
Casey Rodarmor <casey@rodarmor.com>
2026-05-22 21:58:11 -0700

    Enhance

--- crates/boilerplate-tests/src/lib.rs
+++ crates/boilerplate-tests/src/lib.rs
@@ -24,3 +24,45 @@ struct AutoIndentContext {
 fn foo() {
   boilerplate::boilerplate!("Hello!");
 }
+
+#[cfg(test)]
+mod tests {
+  extern crate alloc;
+
+  use alloc::string::ToString;
+
+  #[derive(boilerplate::Boilerplate)]
+  #[boilerplate(
+    axum = false,
+    text = "<div>
+  {{ self.0 }}
+</div>
+"
+  )]
+  struct Block(&'static str);
+
+  #[derive(boilerplate::Boilerplate)]
+  #[boilerplate(
+    axum = false,
+    text = "<div>
+  $$ self.0
+</div>
+"
+  )]
+  struct Line(&'static str);
+
+  #[track_caller]
+  fn equivalent(content: &'static str) {
+    assert_eq!(Block(content).to_string(), Line(content).to_string());
+  }
+
+  #[test]
+  fn block_and_line_are_equivalent() {
+    equivalent("");
+    equivalent("foo");
+    equivalent("foo\n");
+    equivalent("foo\nbar");
+    equivalent("foo\nbar\n");
+    equivalent("foo\nbar\nbaz\n");
+  }
+}
