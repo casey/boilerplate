0b81993c16c47ec2a2bd8b5f3ae418a5e2ab1f55
Casey Rodarmor <casey@rodarmor.com>
2026-05-26 16:35:44 -0700

    Enhance

--- src/format.rs
+++ src/format.rs
@@ -3,6 +3,6 @@ use super::*;
 pub trait Format {
   /// Write `self` to `f`, escaping if necessary, indenting continuation lines
   /// by `indent`, and dropping a single trailing newline from the output if
-  /// `trim`.
+  /// `trim` is true.
   fn format(&self, f: &mut fmt::Formatter, indent: &'static str, trim: bool) -> fmt::Result;
 }
